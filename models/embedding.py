de7ab457d8ee185207050240ca6309b8b49ea451
Siddharth Mishra-Sharma <smsharma@princeton.edu>
2020-10-22 10:12:12 -0400

    Reorganized files

---
+++ models/embedding.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from models.chebyshev import SphericalChebConv
+from models.healpix_pool_unpool import Healpix
+from models.laplacians import get_healpix_laplacians
+from models.layers import SphericalChebBNPool
+
+class SphericalGraphCNN(nn.Module):
+    """Spherical GCNN Autoencoder.
+    """
+
+    def __init__(self, nside_list, indexes_list, kernel_size=4, laplacian_type="combinatorial"):
+        """Initialization.
+
+        Args:
+            pooling_class (obj): One of three classes of pooling methods
+            N (int): Number of pixels in the input image
+            kernel_size (int): chebychev polynomial degree
+        """
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.pooling_class = Healpix(mode="max")
+        
+        self.laps = get_healpix_laplacians(nside_list=nside_list, laplacian_type=laplacian_type, indexes_list=indexes_list)
+
+        self.cnn_layers = []
+        
+        for i, (in_ch, out_ch) in enumerate([(1, 32), (32, 64), (64, 128), (128, 256), (256, 256), (256, 256), (256, 256)]):
+            layer = SphericalChebBNPool(in_ch, out_ch, self.laps[i], self.pooling_class.pooling, self.kernel_size)
+            self.cnn_layers.append(layer)
+
+        self.fc1 = nn.Linear(256, 512)
+        self.fc2 = nn.Linear(512, 64)
+
+    def forward(self, x):
+        """Forward Pass.
+
+        Args:
+            x (:obj:`torch.Tensor`): input to be forwarded.
+
+        Returns:
+            :obj:`torch.Tensor`: output
+        """
+        x = x.view(-1, 16384, 1)
+
+        for layer in self.cnn_layers:
+            x = layer(x)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        return x[:, 0, :]
---
+++ models/layers.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from models.chebyshev import SphericalChebConv
+from models.healpix_pool_unpool import Healpix
+from models.laplacians import get_healpix_laplacians
+
+class SphericalChebBNPool(nn.Module):
+    """Building Block with a pooling/unpooling, a calling the SphericalChebBN block.
+    """
+
+    def __init__(self, in_channels, out_channels, lap, pooling, kernel_size):
+        """Initialization.
+
+        Args:
+            in_channels (int): initial number of channels.
+            out_channels (int): output number of channels.
+            lap (:obj:`torch.sparse.FloatTensor`): laplacian.
+            pooling (:obj:`torch.nn.Module`): pooling/unpooling module.
+            kernel_size (int, optional): polynomial degree. Defaults to 3.
+        """
+        super().__init__()
+        self.spherical_cheb = SphericalChebConv(in_channels, out_channels, lap, kernel_size)
+        self.pooling = pooling
+        self.batchnorm = nn.BatchNorm1d(out_channels)
+
+    def forward(self, x):
+        """Forward Pass.
+
+        Args:
+            x (:obj:`torch.tensor`): input [batch x vertices x channels/features]
+
+        Returns:
+            :obj:`torch.tensor`: output [batch x vertices x channels/features]
+        """
+        x = self.spherical_cheb(x)
+        x = self.batchnorm(x.permute(0, 2, 1))
+        x = F.relu(x.permute(0, 2, 1))
+        x = self.pooling(x)
+        return x
