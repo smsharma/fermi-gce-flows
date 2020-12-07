ae700615359a5a83b314ddd86bda04fe706bb582
Siddharth Mishra-Sharma <smsharma@princeton.edu>
2020-12-07 13:10:20 -0500

    Trying Greene cluster

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=log_simulate_%a.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32GB
-#SBATCH --time=10:00:00
+#SBATCH --mem=4GB
+#SBATCH --time=02:59:00
 # #SBATCH --gres=gpu:1
 
 conda activate
 cd /home/sm8383/sbi-fermi/
 
-python -u simulate.py -n 1000 --name train_${SLURM_ARRAY_TASK_ID} --dir /scratch/sm8383/sbi-fermi/
+python -u simulate.py -n 10000 --name train_${SLURM_ARRAY_TASK_ID} --dir /scratch/sm8383/sbi-fermi/
