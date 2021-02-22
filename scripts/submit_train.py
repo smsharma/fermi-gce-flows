041a0ae78db3f96a3f0f36fa0355389028a0c4d7
Siddharth Mishra Sharma <sm8383@nyu.edu>
2021-02-22 15:30:19 -0500

    Ran some PCA-flows

--- scripts/submit_train.py
+++ scripts/submit_train.py
@@ -14,21 +14,35 @@ conda activate sbi-fermi
 cd /scratch/sm8383/sbi-fermi/
 """
 
+# batch_size_list = [64, 256]
+# fc_dims_list = [[[-1, 2048], [2048, 512], [512, 96]], 
+#                 [[-1, 2048], [2048, 512]]]
+# maf_num_transforms_list = [4]
+# methods = ["snpe", "snre"]
+
+# for maf_num_transforms in maf_num_transforms_list:
+#     for batch_size in batch_size_list:
+#         for fc_dims in fc_dims_list:
+#             for method in methods:
+#                 batchn = batch + "\n"
+#                 batchn += "python -u train.py --sample train_ModelO_gamma_fix --name gce_ModelO_gamma_fix --method {} --maf_num_transforms {} --fc_dims '{}' --batch_size {}".format(method, maf_num_transforms, fc_dims, batch_size)
+#                 fname = "batch/submit.batch"
+#                 f = open(fname, "w")
+#                 f.write(batchn)
+#                 f.close()
+#                 os.system("chmod +x " + fname)
+#                 os.system("sbatch " + fname)
+
 batch_size_list = [64, 256]
-fc_dims_list = [[[-1, 2048], [2048, 512], [512, 96]], 
-                [[-1, 2048], [2048, 512]]]
-maf_num_transforms_list = [4]
-methods = ["snpe", "snre"]
+maf_num_transforms_list = [4, 8]
 
 for maf_num_transforms in maf_num_transforms_list:
     for batch_size in batch_size_list:
-        for fc_dims in fc_dims_list:
-            for method in methods:
-                batchn = batch + "\n"
-                batchn += "python -u train.py --sample train_ModelO_gamma_fix --name gce_ModelO_gamma_fix --method {} --maf_num_transforms {} --fc_dims '{}' --batch_size {}".format(method, maf_num_transforms, fc_dims, batch_size)
-                fname = "batch/submit.batch"
-                f = open(fname, "w")
-                f.write(batchn)
-                f.close()
-                os.system("chmod +x " + fname)
-                os.system("sbatch " + fname)
+            batchn = batch + "\n"
+            batchn += "python -u train.py --sample train_ModelO_gamma_fix --name gce_ModelO_gamma_fix --maf_num_transforms {} --batch_size {} --summary pca_96".format(maf_num_transforms, batch_size)
+            fname = "batch/submit.batch"
+            f = open(fname, "w")
+            f.write(batchn)
+            f.close()
+            os.system("chmod +x " + fname)
+            os.system("sbatch " + fname)
