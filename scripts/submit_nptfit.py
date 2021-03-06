13139e5c294ce0de7a5b5ea4921c80ded048b3d8
Siddharth Mishra Sharma <sm8383@nyu.edu>
2021-03-06 13:45:57 -0500

    Perfectly working NPTF, ptl

--- scripts/submit_nptfit.py
+++ scripts/submit_nptfit.py
@@ -6,8 +6,8 @@ batch = """#!/bin/bash
 #SBATCH --ntasks-per-node=36
 #SBATCH -t 23:59:00
 #SBATCH --mem=24GB
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
+##SBATCH --mail-type=begin
+##SBATCH --mail-type=end
 ##SBATCH --mail-user=sm8383@nyu.edu
 
 module load gsl/intel/2.6 
@@ -17,7 +17,7 @@ cd /scratch/sm8383/sbi-fermi
 """
 
 diffuse_list = ["ModelO", "p6"]
-ps_mask_list = ["0.8", "default"]
+ps_mask_list = ["0.8"]
 transform_prior_on_s_list = [1]
 
 for ps_mask_type in ps_mask_list:
