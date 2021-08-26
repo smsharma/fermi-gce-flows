2e49c01c77f07c56af175519f64e060f77f13d0c
Siddharth Mishra Sharma <sm8383@nyu.edu>
2021-08-26 12:28:44 -0400

    Ran fiducial model with larger dataset size to make sure results don't change

--- scripts/combine_samples.sh
+++ scripts/combine_samples.sh
@@ -4,13 +4,13 @@
 #SBATCH --output=combine.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=400GB
-#SBATCH --time=00:59:00
+#SBATCH --mem=550GB
+#SBATCH --time=01:30:00
 # #SBATCH --gres=gpu:1
 
 source ~/.bashrc
 conda activate sbi-fermi
 cd /scratch/sm8383/sbi-fermi
 
-./combine_samples.py --regex train_ModelA_gamma_fix_thin_disk_wider_priors_1M 'train_ModelA_gamma_fix_thin_disk_wider_priors_\d+' --dir /scratch/sm8383/sbi-fermi/
+./combine_samples.py --regex train_ModelO_gamma_fix_thin_disk_1p3M 'train_ModelO_gamma_fix_thin_disk_\d+' --dir /scratch/sm8383/sbi-fermi/
 
