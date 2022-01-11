b16580eac326aad01105726216441f6e50f576fb
Siddharth Mishra-Sharma <smsharma@princeton.edu>
2022-01-11 11:32:32 -0500

    NPTF runs with mismodeling

--- scripts/submit_nptfit.py
+++ scripts/submit_nptfit.py
@@ -13,21 +13,43 @@ batch = """#!/bin/bash
 module load gsl/intel/2.6 
 conda activate sbi-fermi
 
-cd /scratch/sm8383/sbi-fermi
+cd /scratch/sm8383/fermi-gce-flows
 """
 
+# # Runs on data
+
+# # Various configurations
+# sample_list = ["fermi_data_thin_disk_1000", "fermi_data_ModelO_1000", "fermi_data_thin_disk_ModelA_1000", "fermi_data_thin_disk_ModelF_1000", "fermi_data_thin_disk_new_ps_priors_1000"]
+# new_ps_priors_list = [0, 0, 0, 0, 1]
+# disk_type_list = ["thin", "thick", "thin", "thin", "thin"]
+# diffuse_list = ["ModelO", "ModelO", "ModelA", "ModelF", "ModelO"]
+
+# for sample_name, new_ps_priors, disk_type, diffuse in zip(sample_list, new_ps_priors_list, disk_type_list, diffuse_list):
+#     batchn = batch + "\n"
+#     batchn += "python nptfit.py --sample_name {} --n_cpus 24 --r_outer 25 --n_live 1000 --disk_type {} --i_mc -1 --diffuse {} --new_ps_priors {}".format(sample_name, disk_type, diffuse, new_ps_priors)
+#     fname = "batch/submit.batch"
+#     f = open(fname, "w")
+#     f.write(batchn)
+#     f.close()
+#     os.system("chmod +x " + fname)
+#     os.system("sbatch " + fname)
+
+
+# Tests on simulations
+
 # Various configurations
-sample_list = ["fermi_data_thin_disk_1000", "fermi_data_ModelO_1000", "fermi_data_thin_disk_ModelA_1000", "fermi_data_thin_disk_ModelF_1000", "fermi_data_thin_disk_new_ps_priors_1000"]
-new_ps_priors_list = [0, 0, 0, 0, 1]
-disk_type_list = ["thin", "thick", "thin", "thin", "thin"]
-diffuse_list = ["ModelO", "ModelO", "ModelA", "ModelF", "ModelO"]
+sample_list = ["ModelO_DM_only_mismo"]
+new_ps_priors_list = [0]
+disk_type_list = ["thin"]
+diffuse_list = ["ModelO"]
 
 for sample_name, new_ps_priors, disk_type, diffuse in zip(sample_list, new_ps_priors_list, disk_type_list, diffuse_list):
-    batchn = batch + "\n"
-    batchn += "python nptfit.py --sample_name {} --n_cpus 24 --r_outer 25 --n_live 1000 --disk_type {} --i_mc -1 --diffuse {} --new_ps_priors {}".format(sample_name, disk_type, diffuse, new_ps_priors)
-    fname = "batch/submit.batch"
-    f = open(fname, "w")
-    f.write(batchn)
-    f.close()
-    os.system("chmod +x " + fname)
-    os.system("sbatch " + fname)
+    for i_mc in range(5):
+        batchn = batch + "\n"
+        batchn += "python nptfit.py --sample_name {} --n_cpus 24 --r_outer 25 --n_live 1000 --disk_type {} --i_mc {} --diffuse {} --new_ps_priors {}".format(sample_name,  disk_type, i_mc, diffuse, new_ps_priors)
+        fname = "batch/submit.batch"
+        f = open(fname, "w")
+        f.write(batchn)
+        f.close()
+        os.system("chmod +x " + fname)
+        os.system("sbatch " + fname)
